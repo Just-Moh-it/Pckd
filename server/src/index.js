4d3baa817c18954329863fece04c9183cc76a465
Mohit Yadav <www.mohit2004@gmail.com>
2021-12-28 23:08:51 +0530

    🎊 Added server hosting to app

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ const { ApolloServer } = require("apollo-server-express");
 const { ApolloServerPluginDrainHttpServer } = require("apollo-server-core");
 const express = require("express");
 const { getUserId } = require("./utils/auth");
+const path = require("path");
 const http = require("http");
 
 // Config Imports
@@ -37,6 +38,21 @@ const prisma = new PrismaClient();
   server.applyMiddleware({ app });
 
   app.use("/static", express.static("static"));
+
+  // Serve react app
+  const rootRouter = express.Router();
+  /*
+   * all your other routes go here
+   */
+  const buildPath = path.normalize(
+    path.join(__dirname + "../../../client/build")
+  );
+  app.use(express.static(buildPath));
+
+  rootRouter.get("(/*)?", async (req, res, next) => {
+    res.sendFile(path.join(buildPath, "index.html"));
+  });
+  app.use(rootRouter);
 
   await new Promise((resolve) => httpServer.listen({ port: PORT }, resolve));
   console.log(
