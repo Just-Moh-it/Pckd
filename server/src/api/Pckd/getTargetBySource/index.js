4f8057a98c153d58f9d3ef55b76b7da6be8403a4
Mohit Yadav <www.mohit2004@gmail.com>
2022-02-03 22:08:29 +0530

    🥰 Updated README and added backlogged updates

--- server/src/api/Pckd/getTargetBySource/index.js
+++ server/src/api/Pckd/getTargetBySource/index.js
@@ -88,21 +88,20 @@ const handleHitInsert = async (ctx, id) => {
           locationContinentCode: rawIspInfo.location.continent.code,
         };
       } else ipInfo = {};
-
-      // 3.1.3 Insert isp info into database
-      await prisma.hits.create({
-        data: {
-          pckdId: id,
-          ...ipInfo,
-          browserName: browserName,
-          browserVersion: browserVersion,
-          OSName: OSName,
-          OSVersion: OSVersion,
-        },
-      });
     } catch (error) {
       console.error("IP address error: ", error);
     }
+    // 3.1.3 Insert isp info into database
+    await prisma.hits.create({
+      data: {
+        pckdId: id,
+        ...ipInfo,
+        browserName: browserName,
+        browserVersion: browserVersion,
+        OSName: OSName,
+        OSVersion: OSVersion,
+      },
+    });
   } catch (err) {
     console.error("An ip address resolving error occured:", err);
   }
